13657e52308041c254b324c5f38ff0a0a0aab5cc
Britjeans <bridgetzhangchina@gmail.com>
2018-01-30 11:25:12 +0800

    Update read_record.py

--- read_record.py
+++ read_record.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 from PIL import Image
-if __name__=='__main__':
-    tfrecords_filename = "flowers_train_0000-of-0001.tfrecord"
-    filename_queue = tf.train.string_input_producer([tfrecords_filename],) 
+import numpy as np
+
+def read_and_decode(filename_queue):
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)   
     features = tf.parse_single_example(serialized_example,
@@ -10,20 +10,37 @@ if __name__=='__main__':
                                            'image/feature': tf.FixedLenFeature((128), tf.float32),
                                            'image/encoded' : tf.FixedLenFeature([], tf.string),
                                        })  
-    image = tf.image.decode_jpeg(features['image/encoded'])
+    image=features['image/encoded']
+    image_decoded = tf.image.decode_jpeg(image)
+	feature = tf.cast(features['image/feature'], tf.float32)
+	return image_decoded,feature
+
+
+if __name__=='__main__':
+    tfrecords_filename = "flowers_train_0000-of-0001.tfrecord"
+
     #image = tf.reshape(image, [224, 224, 3])
    # image = tf.reshape(image, [7,30])
-    feature = tf.cast(features['image/feature'], tf.float32)
+   
     with tf.Session() as sess: 
+	    filename_queue = tf.train.string_input_producer([tfrecords_filename]) 
+        image,feature=read_and_decode(filename_queue)
+        image.set_shape([160,160,3])
+
         init_op = tf.initialize_all_variables()
         sess.run(init_op)
         coord=tf.train.Coordinator()
         threads= tf.train.start_queue_runners(coord=coord)
-        example, l = sess.run([image,feature])
+       
         #img=Image.fromarray(example, 'RGB')
         #img.save('./'+str(i)+'_''Label_'+str(l)+'.jpg')
-        #print(example, l)
-        print(l.shape)
-        print(example.shape)
+       # print(example, l)
+	    for i in range(4):
+		    example, l = sess.run([image,feature])
+			print('image')
+			print (example)
+			print('feature')
+			print(l)
+    
         coord.request_stop()
         coord.join(threads)
